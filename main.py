d55bb2df184653f62e51368a0f1dae9526eff2fb
Flemming-DC <flem1431@hotmail.com>
2024-09-16 03:12:28 +0200

    m

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from engine.externals.database_py.forest import Forest
 
 
 if __name__ == '__main__':
-    # 'A' is a table tree containing (amongst others) the columns 
-    # a0_1, a0_2, a00_2, a10_2, a20_2, a_1
+    
     sapi_query = """
         WITH cte AS (
             SELECT COL0_1, COL0_2, COL00_2 FROM TREE
@@ -19,15 +18,13 @@ if __name__ == '__main__':
         FROM cte 
         join TREE ON TREE.COL_1 = cte.COL0_1
         """
-    # trees = [...]
-    # trees = load_trees_from_db(connection_info, dialect)
-    # forest.from(trees) # evt. also dialect
-    # forest.from(connection_info, dialect)
+    
     with open('../sapi_secret/pg_password.txt') as f:
         password = f.read()
     forest = Forest.from_database(host='localhost', port = 5432, dbname = 'postgres', user = 'postgres', password=password)
 
-    sql = parser.parse(sapi_query, forest) # datamodel
+    sql = parser.parse(sapi_query, forest)
+
     print("--- sql-str ---")
     print(sql)
     
