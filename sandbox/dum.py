64a1db4b7344827a8b4a8e20ee4f1836201ed214
Flemming-DC <flem1431@hotmail.com>
2024-09-19 00:28:43 +0200

    m

--- sandbox/dum.py
+++ sandbox/dum.py
@@ -1,24 +1,12 @@
 
-from typing import Callable
-from dataclasses import dataclass
+if x := [1]:
+    print(f'x = {x}')
 
-@dataclass
-class Dialect:
-    dialect_str: str
-    blank_from_clause: str
-    columns_query: str
-    foreign_keys_query: str
-    connect: Callable
 
-def foo(): ...
 
-postgress = Dialect(
-    dialect_str = "dum",
-    blank_from_clause = "dum",
-    columns_query = "dum",
-    foreign_keys_query = "dum",
-    connect = foo,
-)
-print(postgress)
+
+
+
+
 
 
